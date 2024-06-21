2e6fb4bb575295e7f2ed8f0d997f639c56d775de
AdrianLona <adrian.lona@mscsdirect.com>
2024-06-21 09:01:04 -0600

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adrianmsc.github.io",
-  base: "mi-repo",
+  base: "",
   integrations: [
     starlight({
       title: "MSC",
